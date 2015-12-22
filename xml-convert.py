2207a094a3c14f7796beed9667b3d285a2754088
Matt Bonner <mbonner@apple.com>
2015-12-22 00:05:00 -0800

    Started stuffing XBRL data into dictionaries that are sorted by their context ID's.  Next step should be to figure out the current period context and return only that context's dictionary to the caller.

--- xml-convert.py
+++ xml-convert.py
@@ -38,7 +38,13 @@ def extractNamespace(key, namespaceDict):
 
     return nsValue
 
-def parseFiling(inputFilename, namespaceDict):
+def parseFiling(inputFilename):
+
+    XBRLroot, namespaceDict = parse_and_get_ns(inputFilename)
+
+    pp = pprint.PrettyPrinter(indent = 4)
+    print("Got XBRLroot "+str(XBRLroot)+"\nNamespaces:")
+    pp.pprint(namespaceDict)
 
     # Generally Accepted Accounting Practices
     usGAAPns = extractNamespace('us-gaap', namespaceDict)
@@ -46,38 +52,77 @@ def parseFiling(inputFilename, namespaceDict):
     # Document and Entity Information
     DEIns = extractNamespace('dei', namespaceDict)
 
-    for event, elem in ET.iterparse(inputFilename):
-        tag = elem.tag
-        if usGAAPns in tag:
-            GAAPterm = tag.replace('{'+usGAAPns+'}', '', 1)
-            GAAPtext = elem.text
-            if GAAPtext != None:
-                if len(GAAPtext) > 100:
-                   GAAPtext = GAAPtext[0:100] + "..."
-                print("GAAP term " + GAAPterm + " " + GAAPtext + " contextRef " + elem.attrib.get('contextRef'))
+    contextID = None
+    ContextDataDict = {}
+    events = "start", "start-ns", "end"
+    root = None
+    namespaceDict = {}
+
+    for event, elem in ET.iterparse(inputFilename, events):
+        if event == "start-ns":
+            if elem[0] in namespaceDict and namespaceDict[elem[0]] != elem[1]:
+                # NOTE: It is perfectly valid to have the same prefix refer
+                #     to different URI namespaces in different parts of the
+                #     document. This exception serves as a reminder that this
+                #     solution is not robust.    Use at your own peril.
+                # raise KeyError("Duplicate prefix with different URI found.")
+                print("Found definition for " + elem[0] + " when it's already defined as " + namespaceDict[elem[0]])
+
+            if len(elem[0]) > 0:
+                namespaceDict[elem[0]] = elem[1]
+
+        elif event == "start":
+            tag = elem.tag
+
+            if root is None:
+                root = elem
+
+            if "context" in tag:
+                contextID = elem.get('id')
+                print("Found context start "+tag+" id "+contextID)
+
+        elif event == "end":
+            tag = elem.tag
+            if usGAAPns in tag:
+                GAAPterm = tag.replace('{'+usGAAPns+'}', '', 1)
+                GAAPtext = elem.text
+                if GAAPtext != None:
+                    if len(GAAPtext) > 100:
+                       GAAPtext = GAAPtext[0:100] + "..."
+                    GAAPContextRef = elem.attrib.get('contextRef')
+                    dataDict = None
+                    if GAAPContextRef in ContextDataDict:
+                        dataDict = ContextDataDict[GAAPContextRef]
+                    else:
+                        dataDict = {}
+                        ContextDataDict[GAAPContextRef] = dataDict
+                    print("GAAP term " + GAAPterm + " " + GAAPtext + " contextRef " + GAAPContextRef)
+                    dataDict[GAAPterm] = GAAPtext
+                else:
+                    print(GAAPterm + " has no text")
+            elif DEIns in tag:
+                DEIterm = tag.replace('{'+DEIns+'}', '', 1)
+                DEItext = elem.text
+                if DEItext != None:
+                    if len(DEItext) > 100:
+                       DEItext = DEItext[0:100] + "..."
+                    print("DEI term " + DEIterm + " " + DEItext)
+                else:
+                    print(DEIterm + " has no text")
+            elif "context" in tag:
+                print("Found context end "+tag)
+                contextID = None
             else:
-                print(GAAPterm + " has no text")
-        elif DEIns in tag:
-            DEIterm = tag.replace('{'+DEIns+'}', '', 1)
-            DEItext = elem.text
-            if DEItext != None:
-                if len(DEItext) > 100:
-                   DEItext = DEItext[0:100] + "..."
-                print("DEI term " + DEIterm + " " + DEItext)
-            else:
-                print(DEIterm + " has no text")
-        elif "context" in tag:
-            print("Found context "+tag)
-        else:
-            OtherTerm = tag
-            OtherText = elem.text or ""
-            OtherText = OtherText.strip()
-            if len(OtherText) > 0:
-                if len(OtherText) > 100:
-                   OtherText = OtherText[0:100] + "..."
-                print("Other term " + OtherTerm + " text '" + OtherText + "'")
-            else:
-                print(OtherTerm + " has no text")
+                OtherTerm = tag
+                OtherText = elem.text or ""
+                OtherText = OtherText.strip()
+                if len(OtherText) > 0:
+                    if len(OtherText) > 100:
+                       OtherText = OtherText[0:100] + "..."
+                    print("Other term " + OtherTerm + " text '" + OtherText + "'")
+                else:
+                    print(OtherTerm + " has no text")
+    print("Context dict of data dicts has " + str(ContextDataDict))
 
 def main():
     if len(sys.argv) < 2:
@@ -86,20 +131,10 @@ def main():
 
     inputFilename = sys.argv[1]
 
-    XBRLroot, namespaces = parse_and_get_ns(inputFilename)
-
-    pp = pprint.PrettyPrinter(indent = 4)
-    print("Got XBRLroot "+str(XBRLroot)+"\nNamespaces:")
-    pp.pprint(namespaces)
-
     # conn = psycopg2.connect(database="stocks_us", user="postgres", password="v3gBdD#VI")
     # cur = conn.cursor()
 
-    # for prefix in namespaces:
-    #     print("Registering "+prefix + " : " + namespaces[prefix])
-    #     ET.register_namespace(prefix, namespaces[prefix])
-
-    parseFiling(inputFilename, namespaces)
+    parseFiling(inputFilename)
 
 
     # cur.close()
